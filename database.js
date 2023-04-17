035f280aa92119913262c478d5341a07854a218e
OscarHagman <oscar.leslie.hagman@gmail.com>
2023-04-17 22:34:17 +0200

    clv's

--- database.js
+++ database.js
@@ -4,6 +4,11 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+console.log('MYSQLDATABASE:', process.env.MYSQLDATABASE)
+console.log('MYSQLUSER:', process.env.MYSQLUSER)
+console.log('MYSQLPASSWORD:', process.env.MYSQLPASSWORD)
+console.log('MYSQLHOST:', process.env.MYSQLHOST)
+
 const sequelize = new Sequelize(process.env.MYSQLDATABASE, process.env.MYSQLUSER, process.env.MYSQLPASSWORD, {
   host: process.env.MYSQLHOST,
   dialect: 'mysql',
