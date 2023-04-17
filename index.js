188065986a1450a121a00f65821c312cff79dccd
OscarHagman <oscar.leslie.hagman@gmail.com>
2023-04-17 22:20:44 +0200

    created db connection with ORM

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ import express from 'express'
 import helmet from 'helmet'
 import bodyParser from 'body-parser'
 import cors from 'cors'
-import fs from 'fs'
+
+import sequelize from './database.js';
+import Sample from './model.js';
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -36,7 +38,40 @@ app.post('/post', (req, res) => {
   res.status(200).send({ res: req.body })
 })
 
-app.listen(port, '0.0.0.0', () => {
-  console.log(`Test server listening on port ${port}`)
+// MySQL
+app.get('/db/get-all-todos', async (req, res) => {
+  const samples = await Sample.findAll();
+  res.status(200).json(samples);
 })
+
+app.post('/db/create-todo', async (req, res) => {
+  try {
+    const { name, description, done } = req.body;
+    const newSample = await Sample.create({ name, description, done });
+    res.status(201).json(newSample);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Error inserting data into the table' });
+  }
+})
+
+app.put('/db/update-todo', async (req, res) => {
+
+})
+
+app.delete('/db/delete-todo', async (req, res) => {
+
+})
+
+sequelize
+  .sync({ force: true }) // Use { force: true } only for development. It drops and recreates tables on every start.
+  .then(() => {
+    console.log('Database synced');
+    app.listen(port, '0.0.0.0', () => {
+      console.log(`Test server running on port ${port}`)
+    })
+  })
+  .catch((error) => console.log('Error syncing database:', error));
+
+
 
---
+++ model.js
@@ -0,0 +1,23 @@
+import { DataTypes } from 'sequelize';
+import sequelize from './database.js';
+
+const Sample = sequelize.define('Sample', {
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.STRING,
+  },
+  done: {
+    type: DataTypes.BOOLEAN,
+  },
+});
+
+export default Sample;
